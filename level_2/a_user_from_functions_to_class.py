4b9055c601d70c1e00cbc5df25fafb3d57f87505
Mark Proskurin <growberry.mark@gmail.com>
2023-09-14 09:28:30 +0300

    level_2 is done

--- level_2/a_user_from_functions_to_class.py
+++ level_2/a_user_from_functions_to_class.py
@@ -5,14 +5,23 @@
     1. Создайте класс User и перенесите всю логику работы с пользователем туда.
 """
 
-
-def make_username_capitalized(username: str):
-    return username.capitalize()
-
-
-def generate_short_user_description(username: str, user_id: int, name: str):
-    return f'User with id {user_id} has {username} username and {name} name'
-
-
 class User:
-    pass  # код писать тут
+    def __init__(self, username: str, user_id: int, name: str):
+        self.username = username
+        self.user_id = user_id
+        self.name = name
+
+
+    def make_username_capitalized(self):
+        return self.username.capitalize()
+
+
+    def generate_short_user_description(self):
+        return f'User with id {self.user_id} has {self.username} username and {self.name} name'
+
+
+if __name__ == '__main__':
+    user = User(username='login', user_id=1, name='first_name')
+    user.username = user.make_username_capitalized()
+    description = user.generate_short_user_description()
+    print(description)
--- level_2/b_user_should_be_banned.py
+++ level_2/b_user_should_be_banned.py
@@ -11,4 +11,45 @@ SURNAMES_TO_BAN = ['Vaughn', 'Wilhelm', 'Santaros', 'Porter', 'Smith']
 
 
 class User:
-    pass  # код писать тут
+    def __init__(self, first_name: str, last_name: str, age: int):
+        self.first_name = first_name
+        self.last_name = last_name
+        self.age = age
+    
+    def should_be_banned(self):
+        if self.last_name in SURNAMES_TO_BAN:
+            return True
+        else:
+            return False
+        
+if __name__ == '__main__':
+    user_1 = User(first_name='Jay', last_name='Vaughn', age=22)
+    user_2 = User(first_name='Bob', last_name='Wilhelm', age=30)
+    user_3 = User(first_name='Steave', last_name='Cubin',age=29)
+    user_4 = User(first_name='Alex', last_name='Santaros', age=25)
+    
+    if user_1.should_be_banned():
+        print(f'{user_1.first_name} {user_1.last_name} should be banned')
+  
+    else:
+        print(f'{user_1.first_name} {user_1.last_name} is allowed')
+        
+    if user_2.should_be_banned():
+        print(f'{user_2.first_name} {user_2.last_name} should be banned')
+    
+    else:
+        print(f'{user_2.first_name} {user_2.last_name} is allowed')
+        
+    if user_3.should_be_banned():
+        print(f'{user_3.first_name} {user_3.last_name} should be banned')
+        
+    else:
+        print(f'{user_3.first_name} {user_3.last_name} is allowed')
+        
+    if user_4.should_be_banned():
+        print(f'{user_4.first_name} {user_4.last_name} should be banned')
+        
+    else:
+        print(f'{user_4.first_name} {user_4.last_name} is allowed')
+        
+
